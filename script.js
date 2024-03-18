5ba186ad208608f83dde64862a3677eebbf86ef9
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-18 23:12:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,4 +16,26 @@ async function checkWeather(city){
     const api_key = "4cd0eee81294c867b4bc4cfc64e998c5";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
+    const weather_data = await fetch(`${url}`).then(response => response.json());
+
+
+    if(weather_data.cod === `404`){
+        location_not_found.style.display = "flex";
+        weather_body.style.display = "none";
+        console.log("error");
+        return;
+    }
+
+    console.log("run");
+    location_not_found.style.display = "none";
+    weather_body.style.display = "flex";
+    temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+    description.innerHTML = `${weather_data.weather[0].description}`;
+
+    humidity.innerHTML = `${weather_data.main.humidity}%`;
+    wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
+
+
+    switch(weather_data.weather[0].main){
+        case 'Clouds':
   
