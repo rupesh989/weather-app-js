07a49cee3efeeb359afed2de3a4c89c453d5d545
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-18 23:12:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,4 +38,28 @@ async function checkWeather(city){
 
     switch(weather_data.weather[0].main){
         case 'Clouds':
-  
+            weather_img.src = "/assets/cloud.png";
+            break;
+        case 'Clear':
+            weather_img.src = "/assets/clear.png";
+            break;
+        case 'Rain':
+            weather_img.src = "/assets/rain.png";
+            break;
+        case 'Mist':
+            weather_img.src = "/assets/mist.png";
+            break;
+        case 'Snow':
+            weather_img.src = "/assets/snow.png";
+            break;
+
+    }
+
+    console.log(weather_data);
+}
+
+
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(inputBox.value);
+});
+
